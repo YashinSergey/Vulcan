22b16ccc58eb6cd24c706d0e6bd3a3ef0b014bb8
mr.Blue <yashinsergey125@gmail.com>
2019-12-12 17:50:54 +0300

    modified shouldOverrideUrlLoading

--- app/src/main/java/com/example/vulcan/ui/MyWebViewClient.java
+++ app/src/main/java/com/example/vulcan/ui/MyWebViewClient.java
@@ -1,19 +1,27 @@
 package com.example.vulcan.ui;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.google.android.material.snackbar.Snackbar;
 
 public class MyWebViewClient extends WebViewClient {
 
     private ProgressDialog progressDialog;
+    private Context context;
+    private Activity activity;
 
-    public MyWebViewClient(ProgressDialog progressDialog) {
+    public MyWebViewClient(ProgressDialog progressDialog, Context context, Activity activity) {
         this.progressDialog = progressDialog;
+        this.context = context;
+        this.activity = activity;
         progressDialog.show();
     }
 
@@ -25,9 +33,15 @@ public class MyWebViewClient extends WebViewClient {
 
     @TargetApi(Build.VERSION_CODES.N) @Override
     public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        if (request.getUrl().toString().equals("fail")) {
+            Snackbar.make(view, "shouldOverrideUrlLoading", Snackbar.LENGTH_SHORT).show();
+            Intent intent = new Intent(context, RegistrationActivity.class);
+            activity.startActivity(intent);
+        }
         view.loadUrl(request.getUrl().toString());
         return true;
     }
+
 
     @Override
     public void onPageFinished(WebView view, String url) {
